f3f73445f4ccbc4f09f685a515c23acb79932e40
grafijoss <grafijoss@hotmail.com>
2020-09-04 23:09:14 -0500

    Loadable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
-import Prueba from './components/Prueba'
-import Invoices from './components/Invoices'
+import Loadable from 'react-loadable'
+// import Prueba from './components/Prueba'
+// import Invoices from './components/Invoices'
 import './App.css';
+
+const Loader = x => Loadable({
+	loading: () => 'Cargando...',
+	loader: x
+})
+
+// Dinamic Import
+const Prueba = Loader(() => import('./components/Prueba'))
+const Invoices = Loader(() => import('./components/Invoices'))
 
 function App() {
 	return (
